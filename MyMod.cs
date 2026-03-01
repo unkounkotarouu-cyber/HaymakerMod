bff931e60e190e8473b9e3af9512110484cf7218
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-01 21:21:52 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -1,8 +1,25 @@
+using MelonLoader;
+using HarmonyLib;
+
+[assembly: MelonInfo(typeof(HaymakerMod.MyMod), "Haymaker Musou", "1.0.0", "You")]
+[assembly: MelonGame(null, null)]
+
 namespace HaymakerMod
 {
-    public class MyMod
+    public class MyMod : MelonMod
     {
-        // ログに出す文字だけ決めておくぜ
-        public string ModName = "Haymaker Musou";
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Punch Power x100 Mod: Active!");
+        }
+
+        // ここに「パンチの威力を上書きする」処理を書き足していくぜ
+        // ※まずはビルドを通すための安全な構造だ
+    }
+
+    [HarmonyPatch] // ゲーム内の特定の処理を狙い撃ちするぜ
+    public class PunchPowerPatch
+    {
+        // ここに「敵を殴った時のダメージ計算」を横取りするコードを書く準備
     }
 }
