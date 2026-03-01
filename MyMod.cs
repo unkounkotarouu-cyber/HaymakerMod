fbd2fb056ee59289d4c705242396e0c5067a2dc3
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-01 21:00:01 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -1,7 +1,18 @@
-namespace HaymakerMod
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(MyMod), "Haymaker Musou", "1.0.0", "You")]
+[assembly: MelonGame(null, null)]
+
+public class MyMod : MelonMod
 {
-    public class MyMod 
+    public override void OnInitializeMelon()
     {
-        // まずは「緑のチェック」を出すためだけの空っぽのコードだ
+        MelonLogger.Msg("Musou Mod Loaded! Let's Go!");
+    }
+
+    public override void OnUpdate()
+    {
+        // ここに「攻撃ボタンを押したら敵が飛ぶ」などの無双処理を書いていく
+        // 今はビルドが通ることを優先した構成だぜ！
     }
 }
