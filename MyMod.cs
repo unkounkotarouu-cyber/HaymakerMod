a8f5cb8ffec9abce3f19c2c8cb4cef857ca67947
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-01 20:33:46 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -1,23 +1,13 @@
 using MelonLoader;
 using UnityEngine;
 
-// ここで Mod の情報を定義する（ここが間違ってるとエラーになる）
 [assembly: MelonInfo(typeof(MyMod), "Haymaker Musou", "1.0.0", "You")]
 [assembly: MelonGame(null, null)]
 
-namespace HaymakerMod
+public class MyMod : MelonMod
 {
-    public class MyMod : MelonMod
+    public override void OnInitializeMelon()
     {
-        public override void OnInitializeMelon()
-        {
-            LoggerInstance.Msg("Haymaker Musou Initialized!");
-        }
-
-        public override void OnUpdate()
-        {
-            // ここに無双の処理を書く
-            // 今はテスト用にログが出るようにしてあるぜ
-        }
+        LoggerInstance.Msg("Haymaker Musou Ready!");
     }
 }
