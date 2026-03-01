73bebd7aa99561b26e529ff0514a02f61911bc82
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-01 18:59:31 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -1,15 +1,24 @@
-using MelonLoader;
-using UnityEngine;
+name: Build MelonMod
+on: [push]
 
-[assembly: MelonInfo(typeof(HaymakerMod.MyFirstMod), "GodHand", "1.0.0", "YourName")]
-[assembly: MelonGame("Console Studios", "Haymaker")]
-
-namespace HaymakerMod {
-    public class MyFirstMod : MelonLoaderMod {
-        public override void OnStart() {
-            GameObject button = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            button.transform.position = new Vector3(0, 1.5f, 1f);
-            button.GetComponent<Renderer>().material.color = Color.red;
-        }
-    }
-}
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+    - name: Setup .NET
+      uses: microsoft/setup-dotnet@v4
+      with:
+        dotnet-version: 6.0.x
+    - name: Build
+      run: |
+        dotnet new classlib -n TempProject
+        cd TempProject
+        dotnet add package MelonLoader --version 0.6.1
+        copy ../MyMod.cs ./Class1.cs
+        dotnet build --configuration Release
+    - name: Upload
+      uses: actions/upload-artifact@v4
+      with:
+        name: MyCompiledMod
+        path: TempProject/bin/Release/net6.0/*.dll
