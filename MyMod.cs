32cdf8b1e68b49f518697dde1d3736133a851b06
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-01 20:39:01 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -1,10 +1,12 @@
 using MelonLoader;
-using UnityEngine;
 
 [assembly: MelonInfo(typeof(MyMod), "Haymaker Musou", "1.0.0", "You")]
 [assembly: MelonGame(null, null)]
 
 public class MyMod : MelonMod
 {
-    // これだけでビルドは通る！
+    public override void OnInitializeMelon()
+    {
+        MelonLogger.Msg("Musou Ready!");
+    }
 }
