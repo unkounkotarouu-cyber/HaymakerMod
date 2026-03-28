43b4feb3ac214000b25ba787f761ae18fb8cf4c3
unkounkotarouu-cyber <unkounkotarouu@gmail.com>
2026-03-28 09:19:18 +0900

    Update MyMod.cs

--- MyMod.cs
+++ MyMod.cs
@@ -8,18 +8,24 @@ namespace HaymakerMod
     {
         public override void OnInitializeMelon()
         {
-            MelonLogger.Msg("ULTIMATE PUNCH MODE: ACTIVE!");
+            MelonLogger.Msg("Player Link Power: READY!");
         }
     }
 
     [HarmonyPatch(typeof(Rigidbody), "AddForce", new System.Type[] { typeof(Vector3), typeof(ForceMode) })]
-    public class PunchPowerPatch
+    public class LinkPowerPatch
     {
-        public static void Prefix(ref Vector3 force)
+        public static void Prefix(Rigidbody __instance, ref Vector3 force)
         {
-            // 名前チェックを全部無視して、ゲーム内のあらゆる物理パワーを10倍にするぜ
-            // 100倍だと画面がバグるかもだから、まずは10倍でテストだ！
-            force *= 10f;
+            // お前の「コントローラーに連結されている物」を狙い撃ちするぜ
+            // プレイヤー自身(Player)や、手(Hand)、コントローラー(Controller)という名前が含まれていれば強化
+            if (__instance.gameObject.name.Contains("Player") || 
+                __instance.gameObject.name.Contains("Hand") || 
+                __instance.gameObject.name.Contains("Controller"))
+            {
+                // 威力を 100倍 に！
+                force *= 100f;
+            }
         }
     }
 }
